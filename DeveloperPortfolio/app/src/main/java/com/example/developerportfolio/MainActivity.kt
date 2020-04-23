d4641c8656649ead3c3d322a24294865b0c4428e
Niraj Tiwari <tniraj7@gmail.com>
2020-04-23 18:48:00 +0900

    configure adapter for tab layout and display fragments

--- DeveloperPortfolio/app/src/main/java/com/example/developerportfolio/MainActivity.kt
+++ DeveloperPortfolio/app/src/main/java/com/example/developerportfolio/MainActivity.kt
@@ -1,12 +1,20 @@
 package com.example.developerportfolio
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.developerportfolio.controller.DevPagerAdapter
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        viewPager.adapter = DevPagerAdapter(supportFragmentManager)
+        tabLayoutId.setupWithViewPager(viewPager)
+        tabLayoutId.setTabTextColors(Color.DKGRAY, Color.parseColor("white"))
     }
+
 }
